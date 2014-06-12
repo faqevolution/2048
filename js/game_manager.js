e3355c2aa96fb70d08320db9082abb3864bcafa6
faqevolution <cazanescu.eugeniu@gmail.com>
2014-06-12 15:42:41 +0300

    small changes)
    Signed-off-by: faqevolution <cazanescu.eugeniu@gmail.com>

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,7 +68,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 10: 20;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 10240
+          if (merged.value === 10240) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
